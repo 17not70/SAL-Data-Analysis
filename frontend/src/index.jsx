58bd53e308a4a185fc5283d5f92bdd98ac92242e
Ghanshyam Achayra <17not70@gmail.com>
2025-08-20 09:16:43 +0545

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,7 @@
-// -- RMK: frontend/src/index.js: Final 1.1
+// -- RMK: frontend/src/index.js: Final 1.2
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.js';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
