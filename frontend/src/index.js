fc4d2d6e1bfb60b03ceec419d9d35d6c86e04db7
Ghanshyam Achayra <17not70@gmail.com>
2025-08-20 09:36:15 +0545

    Update and rename index.jsx to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-// -- RMK: frontend/src/index.js: Final 1.3
+// -- RMK: frontend/src/index.js: Final 1.4
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
