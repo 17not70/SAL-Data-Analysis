1e71acc3046569e08f5b0b87acc045536a640fe5
Ghanshyam Achayra <17not70@gmail.com>
2025-08-21 08:57:28 +0545

    Update firebase.js

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,22 +1,19 @@
-// -- RMK: Centralized Firebase configuration and services. Version 1.1
+// -- RMK: Centralized Firebase configuration and services. Version 1.2
 // -- FILE: frontend/src/services/firebase.js
 
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithCustomToken, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'; // Added GoogleAuthProvider and signInWithPopup
+import { getAuth, signInWithCustomToken, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'; // Added signOut
 import { getFirestore, collection, doc, onSnapshot, addDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
-// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
 
-// Initialize and export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
-// Export Firebase functions to be used in other files
 export {
   signInWithCustomToken,
   onAuthStateChanged,
@@ -29,6 +26,7 @@ export {
   ref,
   uploadBytesResumable,
   getDownloadURL,
-  GoogleAuthProvider, // Export new provider
-  signInWithPopup    // Export new sign-in method
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut // Export new sign-out method
 };
